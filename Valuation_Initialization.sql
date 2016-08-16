d6aee07fa1044f7a2616c4cbb396fc743d4ec79a
Joshua Webb <j.curtis.webb@gmail.com>
2016-08-16 17:24:35 -0400

    Fixed Valuation Arithmetic overflow
    The valuation stored procedure will now round off all decimal values to the third place before multiplying and ultimately storing as a smallmoney type.

--- Valuation_Initialization.sql
+++ Valuation_Initialization.sql
@@ -1,7 +1,7 @@
 /* Stored Procedure for Updating Valuation to all Reports */
 CREATE PROC prd.Valuation
-	@rateaboveriskfree AS Decimal(6,4),
-	@riskfree AS Decimal(6,4)
+	@rateaboveriskfree AS smallmoney,
+	@riskfree AS smallmoney
 AS
 BEGIN
 	--If percentages were given instead of decimals, divide percentage by 100
@@ -14,7 +14,7 @@ BEGIN
 				SET @rateaboveriskfree = @rateaboveriskfree /100
 			END
 	UPDATE N2
-		SET N2.Valuation = ROUND((N1.div_growth+1),3)*ROUND(N1.dividend,3)/((@riskfree + @rateaboveriskfree)*N1.Beta - N1.div_growth)
+		SET N2.Valuation = ROUND((N1.div_growth+1),3)*ROUND(N1.dividend,3)/((@riskfree + @rateaboveriskfree)*ROUND(N1.Beta,3) - ROUND(N1.div_growth,3))
 	FROM SQLFinance.prd.Report AS N2
 		INNER JOIN SQLFinance.prd.Report AS N1
 			ON N1.symbol = N2.symbol
